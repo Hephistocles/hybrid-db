3de85a996b174484c4ca8f186eaa423fbd382536
Christopher Little <hephistocles@gmail.com>
2016-01-04 10:26:05 +0000

    Initial Commit

---
+++ data.sh
@@ -0,0 +1,29 @@
+TMP=`mktemp`
+# download data
+# wget -O - -o /dev/null http://www.cc.gatech.edu/dimacs10/archive/data/clustering/cnr-2000.graph.bz2 | bzip2 -d > italy.dat
+# wget -O $TMP -o /dev/null http://files.grouplens.org/datasets/movielens/ml-100k.zip;
+# 	unzip $TMP ml-100k/u.data ml-100k/u.item ml-100k/u.genre ml-100k/u.user ml-100k/u.occupation -d data
+# rm -r data/raw
+# mv data/ml-100k data/raw
+
+mkdir -p data/raw
+
+# get vertices
+echo "id lat lng" > data/raw/points.dat;
+wget -O - http://www.dis.uniroma1.it/challenge9/data/USA-road-d/USA-road-d.NY.co.gz |
+	gunzip |
+	egrep -v '^[cp]' | # strip comments
+	sed 's/^..//' >> data/raw/points.dat;
+
+# get arcs
+echo "id1 id2 dist" > data/raw/edges.dat;
+wget -O - http://www.dis.uniroma1.it/challenge9/data/USA-road-d/USA-road-d.NY.gr.gz |
+	gunzip |
+	egrep -v '^[cp]' | # strip comments
+	sed 's/^..//' >> data/raw/edges.dat;
+
+
+# insert into Neo4J
+neo4j-shell -c < import.cql;
+
+# insert into PostgreSQL
---
+++ install.sh
@@ -0,0 +1,21 @@
+# Install Neo4J
+wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb http://debian.neo4j.org/repo stable/' >/tmp/neo4j.list
+sudo mv /tmp/neo4j.list /etc/apt/sources.list.d
+sudo apt-get update
+
+sudo apt-get install neo4j
+
+
+# Install PostgreSQL
+sudo apt-get install postgresql postgresql-contrib pgadmin3 postgresql-client
+sudo -u postgres createuser --superuser $USER
+sudo -u postgres createdb $USER
+
+echo "To set up the PostgreSQL server password, enter the following:"
+echo ""
+echo "sudo -u postgres psql"
+echo " postgres=# \password $USER"
+echo ""
+
+# create data manipulation groups so the special users have access to what we want
