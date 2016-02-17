a6b1008f48862c18b6ac6552a03c71c0fce6d3e9
Christopher Little <hephistocles@gmail.com>
2016-02-17 11:18:30 +0000

    Refactor connections into consistent promise APIs

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
-function handleResult(result) {
-	console.log(result);
+var mysql = require('promise-mysql');
+var rp = require("request-promise");
+
+main();
+
+
+function init() {
+	return mysql.createConnection({
+		host: 'localhost',
+		user: 'hybrid',
+		database: 'hybrid'
+	})
+	.then(function(conn) {
+		sql.conn = conn;
+	});
 }
 
 
+function main() {
+	init()
+		.then(function() {
+
+			return [
+				sql('select * from points limit 1'),
+				cypher("MATCH (n1)-[r]->(n2) RETURN n1, r, n2 LIMIT 2", {})
+			]
+		})
+		.spread(function(sqlRes, neoRes) {
+			console.log(sqlRes);
+			console.log(neoRes);
+		})
+		.then(function() {
+			sql.conn.end();
+		})
+}
+
+// CONNECTION IMPLEMENTATIONS
+
 // MYSQL
-
-var mysql = require('promise-mysql');
-var connection;
-
-mysql.createConnection({
-	host     : 'localhost',
-	user     : 'hybrid',
-	database : 'hybrid'
-})
-.then(function(conn){
-	connection = conn;
-})
-.then(function() {
-	return connection.query('select * from points limit 1')
-})
-.then(handleResult)
-.then(function(){
-	connection.end();
-});
+function sql(query, params) {
+	return sql.conn.query(query)
+		.then(function(result) {
+			// any default processing?
+			return result;
+		});
+}
 
 
 // NEO4J
-var r=require("request");
-var txUrl = "http://localhost:7474/db/data/transaction/commit";
-function cypher(query,params,cb) {
-	r.post({
-		uri:txUrl,
-		'auth': {
-			'user': 'neo4j',
-			'pass': 'LockDown1'
-		},
-		json:{statements:[{statement:query,parameters:params}]}},
-		function(err,res) { cb(
-			JSON.stringify(res.body))})
+function cypher(query, params) {
+	return rp.post({
+			uri: "http://localhost:7474/db/data/transaction/commit",
+			'auth': {
+				'user': 'neo4j',
+				'pass': 'LockDown1'
+			},
+			json: {
+				statements: [{
+					statement: query,
+					parameters: params
+				}]
+			}
+		})
+		.then(function(result) {
+			// any default processing?
+			return JSON.stringify(result.results[0]);
+		});
 }
-
-var query="MATCH (n1)-[r]->(n2) RETURN n1, r, n2 LIMIT {limit}"
-var params={limit: 2}
-
-cypher(query,params,handleResult)
